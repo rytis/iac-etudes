4dc01e59c35d17d6665d08aa01ffc5850732e854
Rytis Sileika <rts.pub@pulegium.eu>
2023-10-17 10:18:28 +0100

    set up vpc

---
+++ etudes/dask-cluster/aws/terraform/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = var.region
+}
+
+###############################################################################
+## Init
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 0),
+    cidrsubnet(var.vpc_cidr, 8, 1),
+    cidrsubnet(var.vpc_cidr, 8, 2)
+  ]
+  private_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 3),
+    cidrsubnet(var.vpc_cidr, 8, 4),
+    cidrsubnet(var.vpc_cidr, 8, 5)
+  ]
+  db_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 6),
+    cidrsubnet(var.vpc_cidr, 8, 7),
+    cidrsubnet(var.vpc_cidr, 8, 8)
+  ]
+}
+
+
+###############################################################################
+## VPC
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name               = var.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = local.azs
+  public_subnets     = local.public_subnets
+  private_subnets    = local.private_subnets
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
