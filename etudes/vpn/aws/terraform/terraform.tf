87f76bb815e1f0d4a9036b5da97f8c03d9fbad63
Rytis Sileika <rts.pub@pulegium.eu>
2023-11-09 17:37:43 +0000

    starting VPN etude

---
+++ etudes/vpn/aws/terraform/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.24"
+    }
+  }
+
+  backend "s3" {
+    region         = "us-east-2"
+    key            = "terraform.state"
+    bucket         = "rsi-iac-etudes-vpn"
+    dynamodb_table = "rsi-iac-etudes-vpn"
+  }
+}
