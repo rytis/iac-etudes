0ee9dc448c7a601befdf496735f76ccac68377c6
Rytis Sileika <rts.pub@pulegium.eu>
2023-11-10 12:38:40 +0000

    client/server certs uploaded to cert manager

--- etudes/vpn/aws/terraform/modules/client/main.tf
+++ etudes/vpn/aws/terraform/modules/client/main.tf
@@ -1,7 +1,7 @@
 ##############################################################################
-##
+## Certificates for mutual authentication
 
-## CA
+# -- CA certificate ----------------------------------------------------------
 
 resource "tls_private_key" "ca" {
   algorithm = "RSA"
@@ -19,10 +19,13 @@ resource "tls_self_signed_cert" "ca" {
     "cert_signing",
     "crl_signing"
   ]
+
+  subject {
+    common_name = "ca"
+  }
 }
 
-## Server
-
+# -- Server certificate ------------------------------------------------------
 
 resource "tls_private_key" "server" {
   algorithm = "RSA"
@@ -31,6 +34,10 @@ resource "tls_private_key" "server" {
 
 resource "tls_cert_request" "server" {
   private_key_pem = tls_private_key.server.private_key_pem
+
+  subject {
+    common_name = "server"
+  }
 }
 
 resource "tls_locally_signed_cert" "server" {
@@ -46,3 +53,46 @@ resource "tls_locally_signed_cert" "server" {
     "client_auth"
   ]
 }
+
+resource "aws_acm_certificate" "server" {
+  private_key       = tls_private_key.server.private_key_pem
+  certificate_body  = tls_locally_signed_cert.server.cert_pem
+  certificate_chain = tls_self_signed_cert.ca.cert_pem
+}
+
+# -- Client certificate ------------------------------------------------------
+
+resource "tls_private_key" "client" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_cert_request" "client" {
+  private_key_pem = tls_private_key.client.private_key_pem
+
+  subject {
+    common_name = "client"
+  }
+}
+
+resource "tls_locally_signed_cert" "client" {
+  cert_request_pem      = tls_cert_request.client.cert_request_pem
+  ca_private_key_pem    = tls_private_key.ca.private_key_pem
+  ca_cert_pem           = tls_self_signed_cert.ca.cert_pem
+  validity_period_hours = 87600
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+resource "aws_acm_certificate" "client" {
+  private_key       = tls_private_key.client.private_key_pem
+  certificate_body  = tls_locally_signed_cert.client.cert_pem
+  certificate_chain = tls_self_signed_cert.ca.cert_pem
+}
+
+
