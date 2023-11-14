02ebea91f82f67c2ef8190ce7e5238f89201e329
Rytis Sileika <rts.pub@pulegium.eu>
2023-11-14 13:26:23 +0000

    create vpn endpoint with internet access routes

--- etudes/vpn/aws/terraform/modules/client/main.tf
+++ etudes/vpn/aws/terraform/modules/client/main.tf
@@ -20,6 +20,8 @@ resource "tls_self_signed_cert" "ca" {
     "crl_signing"
   ]
 
+  dns_names = ["ca"]
+
   subject {
     common_name = "ca"
   }
@@ -34,6 +36,8 @@ resource "tls_private_key" "server" {
 
 resource "tls_cert_request" "server" {
   private_key_pem = tls_private_key.server.private_key_pem
+
+  dns_names = ["server"]
 
   subject {
     common_name = "server"
@@ -70,6 +74,8 @@ resource "tls_private_key" "client" {
 resource "tls_cert_request" "client" {
   private_key_pem = tls_private_key.client.private_key_pem
 
+  dns_names = ["client"]
+
   subject {
     common_name = "client"
   }
@@ -95,4 +101,76 @@ resource "aws_acm_certificate" "client" {
   certificate_chain = tls_self_signed_cert.ca.cert_pem
 }
 
+resource "local_sensitive_file" "client_key" {
+  content  = tls_private_key.client.private_key_pem
+  filename = "${path.module}/client.key"
+}
 
+resource "local_file" "client_key" {
+  content  = tls_locally_signed_cert.client.cert_pem
+  filename = "${path.module}/client.cert"
+}
+
+##############################################################################
+##
+
+module "client_vpn_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name   = "client-vpn"
+  vpc_id = var.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["all-all"]
+  # ingress_with_self = ["all-all"]
+  egress_rules = ["all-all"]
+}
+
+resource "aws_ec2_client_vpn_endpoint" "this" {
+  description            = "client VPN"
+  server_certificate_arn = aws_acm_certificate.server.arn
+  client_cidr_block      = "10.100.0.0/22"
+  vpc_id                 = var.vpc.vpc_id
+  security_group_ids     = [module.client_vpn_sg.security_group_id]
+
+  authentication_options {
+    type                       = "certificate-authentication"
+    root_certificate_chain_arn = aws_acm_certificate.client.arn
+  }
+
+  self_service_portal = "enabled"
+
+  connection_log_options {
+    enabled = false
+  }
+}
+
+# resource "aws_ec2_client_vpn_network_association" "this" {
+#   for_each = toset(var.vpc.private_subnets)
+#
+#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this.id
+#   subnet_id              = each.key
+# }
+
+locals {
+  association_subnets = try(var.vpc.public_subnets, tolist([]))
+}
+
+resource "aws_ec2_client_vpn_network_association" "this" {
+  count                  = 3
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this.id
+  subnet_id              = local.association_subnets[count.index]
+}
+
+resource "aws_ec2_client_vpn_authorization_rule" "this" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this.id
+  target_network_cidr    = "0.0.0.0/0"
+  authorize_all_groups   = true
+}
+
+resource "aws_ec2_client_vpn_route" "internet" {
+  count                  = length(local.association_subnets)
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  target_vpc_subnet_id   = local.association_subnets[count.index]
+}
