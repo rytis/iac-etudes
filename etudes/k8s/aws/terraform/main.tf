a8b46d4b2dbfe7438c58b215633e4b8df47a8577
Rytis Sileika <rts.pub@pulegium.eu>
2023-12-15 10:15:18 +0000

    small refactor

--- etudes/k8s/aws/terraform/main.tf
+++ etudes/k8s/aws/terraform/main.tf
@@ -64,28 +64,21 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+    instance_types = ["t3.small"]
+    min_size       = 1
+    max_size       = 3
+    desired_size   = 1
+    iam_role_additional_policies = {
+      efs_full_client = data.aws_iam_policy.aws_efs_full.arn
+    }
   }
 
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 1
-      iam_role_additional_policies = {
-        efs_full_client = data.aws_iam_policy.aws_efs_full.arn
-      }
     }
     two = {
       name           = "node-group-2"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 1
-      iam_role_additional_policies = {
-        efs_full_client = data.aws_iam_policy.aws_efs_full.arn
-      }
     }
   }
 
